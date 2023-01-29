a7c7889771367f8bc261ae8654c38eb2dcac6051
Abbegail Miles <78096484+ms-hollow@users.noreply.github.com>
2023-01-29 17:33:51 +0800

    clean copy

--- team-c-project.asm
+++ team-c-project.asm
@@ -3,30 +3,25 @@ NAME "PRINTER"
 DATA SEGMENT
     
      FIRSTDIGIT DB 1 DUP(?) 
-   
+     TAP_MSG DB 'TAP YOUR CARD'
      CLEARASCII DB "                                                      "
      NUMBERS	DB 00111111B, 00000110B, 01011011B, 01001111B, 01100110B, 01101101B, 01111101B, 00000111B, 01111111B, 01101111B,
-                DB 01110111B, 01111100B, 00111001B, 01011110B, 01111001B, 01110001B    
-     
-     ID DB "0000000.txt",0 
-     ;      01234567 -index   
-
-     filehandler dw ?  
-     handle dw ?                     ;Creates variable to store file handle which identifies the file              
-     TIME_IN db "TIME IN: "          ;creates text to write on file
+                DB 01110111B, 01111100B, 00111001B, 01011110B, 01111001B, 01110001B       
+     ID DB "0000000.txt",0      
+     FILEHANDLER DW ?  
+     HANDLE DW ?                     ;CREATES VARIABLE TO STORE FILE HANDLE WHICH IDENTIFIES THE FILE              
+     TIME_IN DB "TIME IN: "          ;CREATES TEXT TO WRITE ON FILE
      MSG  DB "MAGANDANG ARAW!", 0AH, 0DH
      DB "ANG IYONG ANAK AY LIGTAS NANG NAKARATING SA ", 0AH, 0DH      
      DB "TECHNOLOGICAL UNIVERSITY OF THE PHILIPPINES - MANILA", 0AH, 0DH                                
      DB "MARAMING SALAMAT", 0AH, 0DH
-     DB 13, 9    ; CARRIAGE RETURN AND VERTICAL TAB  
+     DB 13, 9                         ;CARRIAGE RETURN AND VERTICAL TAB  
      SDATE  DB "DATE:" 
-     DATE DB "00\00\00 ", 0 
- ;            01234567 -INDEX  
+     DATE DB "00\00\00", 0 
      SPACE DB " ", 0AH, 0DH 
      PROMPT  DB "TIME:" 
      TIME DB "00:00:00", 0
- ;            01234567 -INDEX  
-     MSG_END db 0      
+     MSG_END DB 0      
 ENDS
 
 STACK SEGMENT
@@ -34,26 +29,21 @@ STACK SEGMENT
 ENDS
 
 CODE SEGMENT 
-
-; CHECK TEMPERATURE     
-CHECK_TEMP   PROC    FAR
-
-    ; STORE RETURN ADDRESS TO OS:
- 	PUSH    DS
+    
+CHECK_TEMP   PROC    FAR    ;CHECK TEMPERATURE 
+ 	PUSH    DS              ;STORE RETURN ADDRESS TO OS:
  	MOV     AX, 0
  	PUSH    AX
 
-    ; SET SEGMENT REGISTERS:
- 	MOV     AX, DATA
+ 	MOV     AX, DATA        ;SET SEGMENT REGISTERS:
  	MOV     DS, AX
  	MOV     ES, AX
 
-    ; INITIALIZE ALL SEVEN SEGMENT DISPLAYS TO EMPTY
-	MOV CX, 8	
+	MOV CX, 8	            ;INITIALIZE ALL SEVEN SEGMENT DISPLAYS TO EMPTY
 	MOV DX, 2030H
 	MOV AL, 00H   
 	
-INIT:	
+INIT:	                    ;INITIALIZE THERMOMETER
     OUT DX, AL
 	INC DX
 	LOOP INIT
@@ -65,8 +55,7 @@ THERMOMETER:
 	MOV BL, AL	            ;BX NOW HAS THE TEMPERATURE
 	MOV BH, 0
 	
-	; DISPLAY TEMPERATURE ON SEVEN SEGMENT (USING HEXADECIMAL)
-	MOV DX, 2030H 	        ;OUTPUT MOST SIGNIFICANT 4-BITS
+	MOV DX, 2030H 	        ;DISPLAY TEMPERATURE ON SEVEN SEGMENT (USING HEXADECIMAL) & OUTPUT MOST SIGNIFICANT 4-BITS
 	MOV SI, BX
 	AND SI, 00F0H
 	MOV CL, 4
@@ -91,39 +80,41 @@ THERMOMETER:
     JL GREEN
     JMP RED
     
-RED:
+RED:                        ;OUTPUT ALL RED COLOR
 	MOV AL, 049H 
     OUT DX, AL 
     JMP EXIT
     
-GREEN:
+GREEN:                      ;OUTPUT ALL GREEN COLOR
     MOV AL, 024H 
     OUT DX, AL     	
-	JMP GET_ID              ;INFINIT LOOP 
+	JMP TAP_INIT            ;INFINIT LOOP 
 	
 CHECK_TEMP  ENDP
 
+TAP_INIT:                   ;INIT ASCII
+	MOV DX, 2040h	 
+	MOV SI, 0
+	MOV CX, 13
 
-; GET INPUT USING KEYBOARD    
-GET_ID:    
-    
- 
-    mov ax, @DATA ;storage ng data segment
-    mov ds,ax
-    lea bx, ID     ;load address ng ID
+DISP_TAP:                   ;DISPLAY TAP MESSAGE
+	MOV AL, TAP_MSG[SI]
+	OUT DX,AL
+	INC SI
+	INC DX
+	LOOP DISP_TAP
+
+GET_ID:                     ;GET INPUT USING KEYBOARD         
+    MOV AX, @DATA           ;STORAGE NG DATA SEGMENT
+    MOV DS,AX
+    LEA BX, ID              ;LOAD ADDRESS NG ID
     MOV CX, 6   
       
-    ;RESET BUFFER INDICATOR TO ALLOW MORE KEYS
-	MOV DX, 2083H
+	MOV DX, 2083H           ;RESET BUFFER INDICATOR TO ALLOW MORE KEYS
 	MOV AL, 00H
 	OUT DX, AL
-	  
-    ;MOV CX, 6
-    ;MOV BX, 2040H
-    
-    ;MAIN    
-    
-	CALL KEYBOARD
+	   
+	CALL KEYBOARD           ;MAIN  
 	mov  dx, offset ID 
     call OPEN_FILE
 	CALL CLEARDISPLAY1
@@ -136,41 +127,39 @@ GET_ID:
 	CALL START_PRINT
 	CALL EXIT  
 	
-KEYBOARD:                          
-                           
-    MOV DX, 2083h 	; input data from keyboard (if buffer has key)
+KEYBOARD:                                                                                        
+    MOV DX, 2083h 	        ;input data from keyboard (if buffer has key)
 	IN  AL, DX    
 	CMP AL, 00h
-	JE  KEYBOARD      	; buffer has no key, check again 
-	
-	; a new key was pressed
-	MOV DX, 2082h	; read key (8-bit input)
+	JE  KEYBOARD      	    ; buffer has no key, check again 
+	                        
+	MOV DX, 2082h	        ; read key (8-bit input)
 	IN  AL, DX	
 
-	aam                 ;ASCII adjust after manipulation- divides AL by 10 and stores quotient to AH, Remainder to AL
-    add ax, 3030h       ;adds 3030h to ax para maging ASCIIang hexadecimal
+	AAM                     ;ASCII adjust after manipulation- divides AL by 10 and stores quotient to AH, Remainder to AL
+    ADD AX, 3030h           ;adds 3030h to ax para maging ASCIIang hexadecimal
 
     MOV [BX], AX 
     INC BX 
     
-    ; reset buffer indicator to allow more keys
-	MOV DX, 2083h
+	MOV DX, 2083h           ;reset buffer indicator to allow more keys
 	MOV AL, 00h
 	OUT DX, AL
 	
     LOOP KEYBOARD
     
-    ;RESET LEDS OUTPUT
-    MOV DX, 2070H
+    MOV DX, 2070H           ;RESET LEDS OUTPUT
     MOV AL, 00H
     OUT DX, AL
-    ;ASCII LCD
-    mov dx,2040h
-    mov si,0
-    mov cx,6
+    CALL CLEARDISPLAY1
+	CALL CLEARDISPLAY2
+  
+    MOV DX,2040H            ;ASCII LCD
+    MOV SI,0
+    MOV CX,6
 
-DISPLAY_ID:
-    mov al,ID[si]; print yung nasa string by their index 
+DISPLAY_ID:                 
+    mov al,ID[si]           ;print yung nasa string by their index 
     out dx,al
     inc si
     inc dx 
@@ -179,31 +168,26 @@ DISPLAY_ID:
     
 OPEN_FILE:
     push ax
-    push bx
-    
+    push bx    
     cmp cl,21d        
     je  CHECK_STATUS
 
-CHECK_STATUS:    .
-    mov al,0             ;READ ONLY MODE.
-    mov ah,03dh          ;SERVICE TO OPEN FILE.
+CHECK_STATUS:               ;CHECH STATUS OF FILE   
+    mov al,0                ;READ ONLY MODE.
+    mov ah,03dh             ;SERVICE TO OPEN FILE.
     int 21h
-    jb  NOTOK            ;ERROR IF CARRY FLAG.
+    jb  NOTOK               ;ERROR IF CARRY FLAG.
     je OK
 
-OK:;DISPLAY OK MESSAGE.
-    mov filehandler, ax  ;IF NO ERROR, NO JUMP. SAVE FILEHANDLER.
-    ;GREEN LIGHT   
-    MOV DX, 2070H 
+OK:                         ;DISPLAY GREEN COLOR IN LED
+    mov filehandler, ax     ;IF NO ERROR, NO JUMP. SAVE FILEHANDLER.  
+    MOV DX, 2070H           ;GREEN LIGHT 
     MOV AL, 024H 
-    OUT DX, AL          
-              
+    OUT DX, AL                       
     jmp endOfFile
 
-NOTOK:;DISPLAY ERROR MESSAGE.
-    
-    ;RED LIGHT
-    MOV DX, 2070H
+NOTOK:                      ;DISPLAY RED COLOR IN LED       
+    MOV DX, 2070H           ;RED LIGHT
     MOV AL, 049H 
     OUT DX, AL
     
@@ -211,20 +195,18 @@ NOTOK:;DISPLAY ERROR MESSAGE.
 	CALL CLEARDISPLAY2
     CALL EXIT
 
-endOfFile:
+endOfFile:                  
     pop bx
     pop ax
     RET
     	
-CLEARDISPLAY1: 
-
+CLEARDISPLAY1:              
 	MOV DX, 2039H
 	MOV SI, 0
 	MOV CX, 48
 	RET
 
 CLEARDISPLAY2:
-
 	MOV AL, CLEARASCII[SI]
 	OUT DX,AL
 	INC SI
@@ -237,42 +219,35 @@ EXIT:
     MOV AH, 4CH
     INT 21H 
     
-    
-; TIME
-	
-DISPLAY_TIME PROC
-    
-     MOV AX, @DATA      ;STORAGE NG DATA SEGMENT
+DISPLAY_TIME PROC           ;FUNCTION FOR TIME    
+     MOV AX, @DATA          ;STORAGE NG DATA SEGMENT
      MOV DS, AX
-     LEA BX, TIME       ;PRINT YUNG STRING TIME 
+     LEA BX, TIME           ;PRINT YUNG STRING TIME 
                       
-     CALL GET_TIME      ;GET TIME 
-     CALL ASCII         ;DISPLAY IN ASCII LCD                           
+     CALL GET_TIME          ;GET TIME 
+     CALL ASCII             ;DISPLAY IN ASCII LCD                           
      RET
      
 DISPLAY_TIME ENDP 
 
-GET_TIME PROC    
-    
+GET_TIME PROC               ;GET TIME                         
     PUSH AX                      
     PUSH CX
                            
-    MOV AH, 2CH         ;GET TIME                
+    MOV AH, 2CH                      
     INT 21H
-
-    ;STORE YUNG HR,TIME,SS SA AL REGISTER
-                           
-    MOV AL, CH          ;HOUR                 
+                         
+    MOV AL, CH              ;HOUR                 
     CALL CONVERT                  
-    MOV [BX], AX        ;ADD YUNG VALUE NG HOUR SA INDEX 0 AT 1
+    MOV [BX], AX            ;ADD YUNG VALUE NG HOUR SA INDEX 0 AT 1
                       
-    MOV AL, CL          ;MINUTE                    
-    CALL CONVERT                  
-    MOV [BX + 3], AX    ;ADD YUNG VALUE NG MINUTE SA INDEX 3AT 4
+    MOV AL, CL              ;MINUTE                    
+    CALL CONVERT                      
+    MOV [BX + 3], AX        ;ADD YUNG VALUE NG MINUTE SA INDEX 3AT 4
                                                              
-    MOV AL, DH          ;SECONDS                
+    MOV AL, DH              ;SECONDS                
     CALL CONVERT                   
-    MOV [BX + 6], AX    ;ADD YUNG VALUE NG MINUTE SA INDEX 6 AT 7
+    MOV [BX + 6], AX        ;ADD YUNG VALUE NG MINUTE SA INDEX 6 AT 7
                                                                        
     POP CX                        
     POP AX                        
@@ -280,27 +255,23 @@ GET_TIME PROC
                                
 GET_TIME ENDP 
 
-;ACII LCD
-
-ASCII PROC
+ASCII PROC                  ;FUNCTION FOR ASCII LCD 
     MOV DX,2040H
     MOV SI,0
     MOV CX,14
     
-NEXT:
-    
-    MOV AL, PROMPT[SI]  ;PRINT YUNG NASA STRING BY THEIR INDEX 
+LOOPHERE:    
+    MOV AL, PROMPT[SI]      ;PRINT YUNG NASA STRING BY THEIR INDEX 
     OUT DX, AL
     INC SI
     INC DX
      
-    LOOP NEXT
+    LOOP LOOPHERE
     RET
 
 ASCII ENDP
 
-CONVERT PROC            ;CONVERTION TO STRING
-         
+CONVERT PROC                ;CONVERTION TO STRING
     PUSH DX                      
     MOV AH, 0                     
     MOV DL, 10                   
@@ -310,38 +281,27 @@ CONVERT PROC            ;CONVERTION TO STRING
     RET
 
 CONVERT ENDP  
-
-; DATE 
    
-GET_DATE:
-
-    TAB EQU 9           ;ASCII CODE  
-    MOV AX, @DATA      ;STORAGE NG DATA SEGMENT
+GET_DATE:                   ;FUNCTION FOR DATE 
+    TAB EQU 9               ;ASCII CODE  
+    MOV AX, @DATA           ;STORAGE NG DATA SEGMENT
     MOV DS, AX
-    LEA BX, DATE       ;PRINT YUNG STRING TIME 
+    LEA BX, DATE            ;PRINT YUNG STRING TIME  
+    MOV AH, 2AH             ;HEXADECIMAL TO GET THE SYSTEM DATE
+    INT 21H                 ;EXECUTES
+    PUSH DX                 ;STORE DX TO STACK 
     
-    MOV AH, 2AH         ;HEXADECIMAL TO GET THE SYSTEM DATE
-    INT 21H             ;EXECUTES
-    
-    ;AFTER THIS, NALAGAY NA SA DESIGNATED REGISTERS ANG DATE
-
-    PUSH DX             ;STORE DX TO STACK 
-    
-;MONTH 
-    MOV AL, DH          ;COPY DH TO AL (SINCE DH CONTAINS THE MONTH AND AL IS THE REGISTER FOR ARITHMETIC)
+    MOV AL, DH              ;MONTH | COPY DH TO AL (SINCE DH CONTAINS THE MONTH AND AL IS THE REGISTER FOR ARITHMETIC) 
     CALL CONVERT_DATE
     MOV [BX], AX 
     
-;DAY 
-    POP DX              ;RETRIEVE DX FROM STACK (DL CONTAINS DAY)
-    MOV AL, DL          ;COPY DL TO AL
+    POP DX                  ;DAY | RETRIEVE DX FROM STACK (DL CONTAINS DAY)
+    MOV AL, DL          
     CALL CONVERT_DATE
     MOV [BX+3], AX
-                
-;YEAR      
-    
-    SUB CX, 2000        ;SUBTRACT 2000 TO CX SINCE IT CNA ONLY STORE TO DIGIT
-    MOV AX, CX          ;COPY CX TO AL (SINCE CX CONTAINS YEAR
+                   
+    SUB CX, 2000            ;YEAR | SUBTRACT 2000 TO CX SINCE IT CNA ONLY STORE TO DIGIT
+    MOV AX, CX         
     CALL CONVERT_DATE
     MOV [BX+6], AX
     MOV DX, 2055H
@@ -349,25 +309,22 @@ GET_DATE:
 	MOV CX, 8 
     RET
     
-CONVERT_DATE:           ;FUNCTION TO CONVERT HEXADECIMAL TO ASCII
-    
-    AAM                 ;ASCII ADJUST AFTER MANIPULATION- DIVIDES AL BY 10 AND STORES QUOTIENT TO AH, REMAINDER TO AL
-    ADD AX, 3030H       ;ADDS 3030H TO AX PARA MAGING ASCIIANG HEXADECIMAL
+CONVERT_DATE:               ;FUNCTION TO CONVERT HEXADECIMAL TO ASCII    
+    AAM                     ;ASCII ADJUST AFTER MANIPULATION- DIVIDES AL BY 10 AND STORES QUOTIENT TO AH, REMAINDER TO AL
+    ADD AX, 3030H           ;ADDS 3030H TO AX PARA MAGING ASCIIANG HEXADECIMAL
     PUSH BX                    
-    MOV BX, AX          ;COPY ANG AX SA BX KASI MABABAGO ANG VALUE NG AX DAHIL SA PRINTING                              
-    MOV AL, BH          ;INTERCHANGE VALUES
+    MOV BX, AX              ;COPY ANG AX SA BX KASI MABABAGO ANG VALUE NG AX DAHIL SA PRINTING                              
+    MOV AL, BH              ;INTERCHANGE VALUES
     MOV AH, BL
     POP BX   
     RET  
 
-INIT_DATE: 
-
+INIT_DATE:                  ;INITIALIZE ASCII LCD
 	MOV DX, 2050H
 	MOV SI, 0
-	MOV CX, 14         ;SINCE 14 ANG CX, PATI KUNG ANO YUNG NASA BABA NG SDATE NAPI-PRINT NIYA, WHICH IS YUNG MISMONG DATE.
+	MOV CX, 14              ;SINCE 14 ANG CX, PATI KUNG ANO YUNG NASA BABA NG SDATE NAPI-PRINT NIYA, WHICH IS YUNG MISMONG DATE.
 
-DISPLAY_DATE:
-
+DISPLAY_DATE:           
 	MOV AL, SDATE[SI]
 	OUT DX,AL
 	INC SI
@@ -375,78 +332,74 @@ DISPLAY_DATE:
 	LOOP DISPLAY_DATE
 	RET  
 
-WRITE_FILE:
+WRITE_FILE:                 ;FUNCTION FOR FILE OPERATION
     mov ax, cs
     mov dx, ax
     mov es, ax
     
-    mov ah, 3ch           ;Function number to create file
+    mov ah, 3ch             ;create file
     mov cx, 0
-    mov dx, offset ID   ;get offset address
+    mov dx, offset ID       ;get offset address
     int 21h
     mov handle, ax   
     
-    mov ah, 40h           ;Function number to write on file
+    mov ah, 40h             ;write on file
     mov bx, handle
-    mov dx, offset SDATE  ;Print sa file yung "TIME IN" " na string
+    mov dx, offset SDATE    ;PRINT "DATE" STRING
     mov cx, 5
     int 21h  
     
-    mov ah, 40h           ;Function number to write on file
+    mov ah, 40h             
     mov bx, handle
-    mov dx, offset DATE   ;Print sa file yung mismong time
+    mov dx, offset DATE     ;DISPLAY DATE
     mov cx, 8
     int 21h
     
-    mov ah, 40h           ;Function number to write on file
+    mov ah, 40h           
     mov bx, handle
-    mov dx, offset TIME_IN ;Print sa file yung "TIME IN" " na string
+    mov dx, offset TIME_IN  ;PRINT "TIME IN"
     mov cx, 9
     int 21h  
     
-    mov ah, 40h           ;Function number to write on file
+    mov ah, 40h          
     mov bx, handle
-    mov dx, offset TIME   ;Print sa file yung mismong time
+    mov dx, offset TIME     ;PRINT TIME
     mov cx, 8
     int 21h
     
-    mov ah, 3eh          ;Function number to close file
+    mov ah, 3eh             ;CLOSE
     mov bx, handle
     int 21h 
     RET         
 
 DISP_GATE PROC    
 
-
-DISP_GATE  ENDP
+DISP_GATE  ENDP             ;DISPLAY GATE NUMBER
     
-    
-    MOV AX, GREETINGS     ;STORAGE NG DATA SEGMENT
+    MOV AX, GREETINGS     
     MOV DS, AX
-    ; CLEAR YUNG PUSH BUTTON    
-    MOV AL, 00h
+      
+    MOV AL, 00h             ;CLEAR PUSH BUTTON 
 	MOV DX, 2080h
 	OUT DX, AL	
 
 INPUT:
-
-	MOV DX, 2080h ; input data from switches
-	IN  AX, DX    ; 16-bit input  
+	MOV DX, 2080h           ;input data from switches
+	IN  AX, DX              ;16-bit input  
  
 	MOV DX, 2000h
 	MOV BX, 00h	 
 	
-	cmp ax, 02h   ;1
+	cmp ax, 02h           
 	je GATE1LOOP
-	cmp ax, 04h   ;2
+	cmp ax, 04h   
 	je GATE2LOOP
 	cmp ax, 08h
 	je GATE3LOOP
 	
 	JNE INPUT 
     
-GATE1LOOP:
-    
+GATE1LOOP:                  ;FUNCTION FOR GATE 1    
 	MOV SI, 0
 
 GATE01:
@@ -462,12 +415,10 @@ GATE01:
 	CMP BX, 40
 	JL GATE1LOOP
 	JE DOTC 
-	
-	
-GATE2LOOP:
+		
+GATE2LOOP:                  ;FUNCTION FOR GATE 2
 	MOV SI, 0
-	;MOV CX, 5
-
+	
 GATE02:
 	MOV AL,Gate2[BX][SI]
 	out dx,al
@@ -482,10 +433,9 @@ GATE02:
 	JL GATE2LOOP
 	JE DOTC 
 	
-GATE3LOOP:
+GATE3LOOP:                  ;FUNCTION FOR GATE 3
 	MOV SI, 0
-	;MOV CX, 5
-
+	
 GATE03:
 	MOV AL,Gate3[BX][SI]
 	out dx,al
@@ -500,8 +450,7 @@ GATE03:
 	JL GATE3LOOP 
 	JE DOTC
 	
-DOTC:	
-
+DOTC:                      ;CLEAR FUNCTION FOR DOT MATRIX	
     MOV DX, 2000h
 	MOV BX, 00h    
  
@@ -523,18 +472,14 @@ CLDISPLAY:
 	CMP BX, 40 
 	JL CLEARLOOP
 
-DOTW:	
-
+DOTW:	                   ;INIT DOT MATRIX
     MOV DX, 2000h
 	MOV BX, 00h
     
-WELLOOP: 
-
+WELLOOP:                   ;FUNCTION FOR WELCOME DISPLAY
 	MOV SI, 0
-	MOV CX, 5  
-	
+  	
 WELDISPLAY:
-
 	MOV AL,Welcome[BX][SI]
 	out dx,al
 	INC SI
@@ -549,11 +494,11 @@ WELDISPLAY:
 	
     RET
 	
-START_PRINT:  
+START_PRINT:                ;FUNCTION FOR PRINTER  
     
-    MOV AX, DATA     ;STORAGE NG DATA SEGMENT
+    MOV AX, DATA     
     MOV DS, AX
-    MOV DL, 12      ; FORM FEED CODE. NEW PAGE.
+    MOV DL, 12              ;FORM FEED CODE. NEW PAGE.
     MOV AH, 5
     INT 21H
 
@@ -562,12 +507,12 @@ START_PRINT:
 
 PRINT:
     MOV DL, [SI]
-    MOV AH, 5       ; MS-DOS PRINT FUNCTION.
+    MOV AH, 5               ;MS-DOS PRINT FUNCTION.
     INT 21H
-    INC SI	        ; NEXT CHAR.
+    INC SI	                ;NEXT CHAR.
     LOOP PRINT
      
-    MOV DL, 12      ; FORM FEED CODE. PAGE OUT!
+    MOV DL, 12              ;FORM FEED CODE. PAGE OUT!
     MOV AH, 5
     INT 21H 
     RET	  
